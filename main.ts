73fdf087d7f5d2dde60954c2fcb1c9c22b083b84
Wei Zhang <kweizh@gmail.com>
2023-01-30 00:47:42 +0800

    :sparkles: done year progress bar
    Signed-off-by: Wei Zhang <kweizh@gmail.com>

--- main.ts
+++ main.ts
@@ -1,81 +1,38 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { parse } from 'yaml'
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface ProgressBarSettings {
+	setting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ProgressBarSettings = {
+	setting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ProgressBar extends Plugin {
+	settings: ProgressBarSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		this.addSettingTab(new ProgressBarSettingTab(this.app, this));
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+
+        this.registerMarkdownCodeBlockProcessor("progressbar", (source, el, ctx) => {
+            const cfg = parse(source);
+            console.log(cfg);
+            if ( !cfg.kind && !cfg.value ) {
+               newError(el, "No kind specified");
+               return;
+            }
+
+            createProgressBar(el, cfg);
+        });
 	}
 
 	onunload() {
@@ -91,26 +48,53 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+function newError(el, msg) {
+    el.createEl("div", { text: 'ProgressBarError: '+msg });
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+const generateUniqueID = (idLength) => [...Array(idLength).keys()].map((elem)=>Math.random().toString(36).substr(2, 1)).join("")
 
-	constructor(app: App, plugin: MyPlugin) {
+function createProgressBar(el, bar) {
+    switch (bar.kind) {
+        case "year":
+            return newYearProgressBar(el, bar);
+        case "month":
+            return newMonthProgressBar(el, bar);
+        case "week":
+            return newWeekProgressBar(el, bar);
+        default:
+    }
+}
+
+function daysIntoYear(date){
+    return (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
+}
+
+function newYearProgressBar(el, bar) {
+    bar.max = new Date().getFullYear() % 4 == 0 ? 366 : 365;
+    bar.value = daysIntoYear(new Date());
+    console.log(bar)
+    newProgressBar(el, bar);
+}
+
+function newProgressBar(el, bar) {
+    const id = generateUniqueID(10);
+    const labelName = bar.name ? bar.name : bar.kind;
+    const label = el.createEl("label", { text: labelName+': ' });
+    label.for = id;
+    const progressbar = el.createEl("progress");
+    progressbar.id = id;
+    progressbar.value = bar.value;
+    progressbar.max = bar.max;
+    if ( bar.width ) {
+        progressbar.style.width = bar.width;
+    }
+}
+
+class ProgressBarSettingTab extends PluginSettingTab {
+	plugin: ProgressBar;
+
+	constructor(app: App, plugin: ProgressBar) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,11 +104,11 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for ProgressBar plugin.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('TBD')
+			.setDesc('Construction')
 			.addText(text => text
 				.setPlaceholder('Enter your secret')
 				.setValue(this.plugin.settings.mySetting)
